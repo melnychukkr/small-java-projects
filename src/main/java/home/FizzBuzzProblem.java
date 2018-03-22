346f52b245d59bb9c26c0b7740e369e00a503a23
karolina_melnuchyk <kuliievakarolina@gmail.com>
2018-03-22 21:28:12 +0100

    Add FizzBuzz problem solution

---
+++ src/main/java/home/FizzBuzzProblem.java
@@ -0,0 +1,26 @@
+package home;
+
+
+public class FizzBuzzProblem {
+    public static void main(String[] args) {
+        String convertedNumber;
+        String fizz;
+        String buzz;
+        for (int i = 1; i <= 100; i++) {
+            convertedNumber = i + "";
+            fizz = "Fizz";
+            buzz = "Buzz";
+            if (i % 5 != 0) {
+                buzz = "";
+            } else {
+                convertedNumber = "";
+            }
+            if (i % 3 != 0) {
+                fizz = "";
+            } else {
+                convertedNumber = "";
+            }
+            System.out.println(convertedNumber + fizz + buzz);
+        }
+    }
+}
